d75c45802cdcb1b4418a423502c0d72687ccf511
nesco <emmanuel@federbusch.fr>
2014-09-20 17:42:25 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,12 @@
+-- main.lua
+
+-- Implements the main plugin entrypoint
+
+
+
+function Initialize(Plugin)
+ 		    Plugin:SetName("Commands") 
+ 		    Plugin:SetVersion(1)
+ 		
+ 		return true
+ end
