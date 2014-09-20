8ce30a99f293ed09a35de3452f0c6d69356460d6
nesco <emmanuel@federbusch.fr>
2014-09-20 17:39:14 +0200

    Create Info.lua

---
+++ Info.lua
@@ -0,0 +1,45 @@
+
+ -- Info.lua
+ 
+ -- Implements the g_PluginInfo standard plugin description
+ 
+ g_PluginInfo = 
+ {
+  
+ 		Name = "Commands"
+ 		Version = "1"
+ 		Date"2014-09-20"
+ 		SourceLocation = ""
+ 		Description = "[[Set of commands]]"
+ 		
+ 		Commands =
+ 		{
+ 		        ["/fireball"] =
+ 		        {
+ 		              Permission = "bc.fireball",
+ 		              Handler = HandleFireballCommand,
+ 		              HelpString = " ~ Shoot a fireball"
+ 		              
+ 		        },
+ 		        
+ 		        ["/thunder"] =
+ 		        {
+ 		              Alias = "/strike",
+ 		              Permission = "bc.thunderbolt",
+ 		              Handler = HandlerThunderboltCommand,
+ 		              HelpString = " ~ Create a thunderbolt",
+ 		              
+ 		        },
+ 		        
+ 		        ["/kick-all"] =
+ 		        {
+ 		              Alias = "/kicka",
+ 		              Permission = "bc.kickall",
+ 		              Handler = HandlerKickAllCommand,
+ 		              HelpString = " ~ Kick all the players",
+ 		              
+ 		        },
+
+ 		}
+ 
+ }
