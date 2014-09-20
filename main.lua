81726934cf91fd6f4d63d6c4c37680da614c179e
nesco <emmanuel@federbusch.fr>
2014-09-20 17:45:51 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,8 +5,10 @@
 
 
 function Initialize(Plugin)
+ 	-- Load the InfoReg shared library:
+	dofile(cPluginManager:GetPluginsPath() .. "/InfoReg.lua")
  		Plugin:SetName("Commands") 
  		Plugin:SetVersion(1)
  		
  		return true
- end
+end
