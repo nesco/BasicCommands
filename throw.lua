68ebf79e53a56f9b6e1ad352fac298652cd51cee
nesco <emmanuel@federbusch.fr>
2014-09-20 20:13:44 +0200

    Correct all the bug

--- throw.lua
+++ throw.lua
@@ -1,35 +1,35 @@
 function HandleThrowCommand(Split, Player)
 		if (Split[2] == nil) then
-				SendMessage(Player, "Usage: " .. Split(1) .. " [arrow|egg|expbottle|fireball|firecharge|skull|snwoball] ")
+				Player:SendMessage("Usage: " .. Split[1] .. " [arrow|egg|expbottle|fireball|firecharge|skull|snowball] ")
 				return true
 		end
 		
 		local Projectile = StringToProjectile(Split[2])
 		if (Projectile == nil) then
-				SendMessage(Player, "Usage: " .. Split(1) .. " [arrow|egg|expbottle|fireball|firecharge|skull|snwoball] ")
+				Player:SendMessage("Usage: " .. Split[1] .. " [arrow|egg|expbottle|fireball|firecharge|skull|snowball] ")
 				return true
 		end
 		
-		local speed = 2
-		local direction = Player:GetLookVector*speed 
-		
-		Player:GetWorld():CreateProjectile(GetBlockXYZFromTrace(Player), Projectile, Player, direction)
-		
+		local speed = 30
+		local direction = Player:GetThrowSpeed(speed)
+		local Item = cItem()
+		local pos = Player:GetThrowStartPos()
+		local id = Player:GetWorld():CreateProjectile(pos.x, pos.y, pos.z, Projectile, Player, Item, direction)
+		return true;
 end
 
 local ProjectileTable =
 {
-		["arrow"]		= pkArrow,
-		["egg"]			= pkEgg,
-		["expbottle"]	= pkExpBottle,
-		["fireball"]	= pkGhastFireball,
-		["firecharge"]	= pkFireCharge,
-		["skull"]		= pkWitherSkull,
-		["snwoball"]	= pkSnowball,
+		["arrow"]		= 60,
+		["egg"]			= 62,
+		["expbottle"]	= 75,
+		["fireball"]	= 63,
+		["firecharge"]	= 64,
+		["skull"]		= 66,
+		["snowball"]	= 61,
 }
 		
 function StringToProjectile(Str)
-
 		local StrLower = string.lower(Str)
 		return ProjectileTable[StrLower]
 end
