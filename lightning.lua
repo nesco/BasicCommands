14114a1a02ac6dd5f5c7b85d13c2fdd7432386ee
nesco <emmanuel@federbusch.fr>
2014-09-25 17:07:03 +0200

    Create lightning.lua

---
+++ lightning.lua
@@ -0,0 +1,20 @@
+function HandleLightningCommand(Split, Player)
+		if #Split == 1 then
+				Player.GetWorld():CastThunderbolt(GetBlockXYZFromTrace(Player))
+				return true
+		elseif #Split == 2 then
+				if Player:HasPermission("bc.lightning.forplayer") then
+						if not strike(Split[2]) then
+							Player:SendMessage(cChatColor.Rose .. "Player couldn't be found.")
+							return true
+						end
+				else
+						Player:SendMessage(cChatColor.Rose .. "You don't have permissions for this command!")
+						return true
+				end
+		end
+		
+		Player:SendMessage(cChatColor.Blue .. "Lightning created!")
+		return true
+end
+					
