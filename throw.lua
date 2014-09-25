e7662f945e736855730539e6f77821926a5f88c9
nesco <emmanuel@federbusch.fr>
2014-09-25 17:00:59 +0200

    Update throw.lua

--- throw.lua
+++ throw.lua
@@ -4,33 +4,13 @@ function HandleThrowCommand(Split, Player)
 				return true
 		end
 		
-		local Projectile = StringToProjectile(Split[2])
-		if (Projectile == nil) then
+		local speed = 30
+		Id = Throw(Player, Split[2], 30)
+			
+		if (Id == false) then
 				Player:SendMessage("Usage: " .. Split[1] .. " [arrow|egg|expbottle|fireball|firecharge|skull|snowball] ")
 				return true
 		end
 		
-		local speed = 30
-		local direction = Player:GetThrowSpeed(speed)
-		local Item = cItem()
-		local pos = Player:GetThrowStartPos()
-		local id = Player:GetWorld():CreateProjectile(pos.x, pos.y, pos.z, Projectile, Player, Item, direction)
 		return true
 end
-
-local ProjectileTable =
-{
-		["arrow"]		= 60,
-		["egg"]			= 62,
-		["expbottle"]	= 75,
-		["fireball"]	= 63,
-		["firecharge"]	= 64,
-		["skull"]		= 66,
-		["snowball"]	= 61,
-}
-		
-function StringToProjectile(Str)
-		local StrLower = string.lower(Str)
-		return ProjectileTable[StrLower]
-end
-		
