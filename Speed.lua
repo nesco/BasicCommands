b3a5c372d4f5a1323d06b923c2e22e8792ab3ae3
nesco <emmanuel@federbusch.fr>
2014-09-27 15:53:19 +0200

    Create Speed.lua

---
+++ Speed.lua
@@ -0,0 +1,23 @@
+function HandleSpeedCommand(Split, Player)
+			if (#Split == 2) then
+					SetGlobalMaxSpeed(tonumber(Split[2]))
+					return true
+					
+			elseif (#Split == 3) then
+					SetMaxSpeed(Player, tonumber(Split[2]), Split[3])
+					return true
+					
+			elseif (#Split == 4) then
+					if not Player:GetWorld():DoWithPlayer(Split[2], SetMaxSpeed(a_Player, tonumber(Split[3], Split[4])
+					) then
+								Player:SendMessage(cChatColor.Rose .. "Player couldn't be found.")
+								return true
+					end
+			end
+			
+			Player:SendMessage(cChatColor.Blue .. "Usage: " .. Split[1] .. " [player] speed [mode]")
+			Player:SendMessage(cChatColor.Blue .. "Mode : walking, flying, sprinting")
+			return true
+end
+					 
+			
